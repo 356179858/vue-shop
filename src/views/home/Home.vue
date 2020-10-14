7dd3d1ec9d798693dbecd590df260d4e0610f765
356179858 <356179858@qq.com>
2020-10-14 23:36:21 +0800

    home修改

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -3,7 +3,7 @@
     <el-header>Header<el-button type="info" @click='logout'>退出</el-button>
     </el-header>
     <el-container>
-      <el-aside width="200px">Aside</el-aside>
+      <el-aside width="200px">左边</el-aside>
       <el-main>Main</el-main>
     </el-container>
   </el-container>
