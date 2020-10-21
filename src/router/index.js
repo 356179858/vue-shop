3fc7ba577ae4b5aa9e18e99acd57d86d85749508
356179858 <356179858@qq.com>
2020-10-21 22:11:34 +0800

    20201021

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 
 import Home from 'views/home/Home.vue'
-import Users from 'views/home/users/Users.vue'
+import Users from 'views/users/Users.vue'
 import Welcome from 'views/home/Welcome.vue'
 import Login from 'views/login/Login.vue'
 
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -5,7 +5,8 @@
     <el-container>
       <el-aside width="200px">
         <!-- 左侧导航 -->
-        <el-menu :background-color="color.backgroundColor" :text-color="color.textColor" :active-text-color="color.activeColor" unique-opened router>
+        <el-menu :background-color="color.backgroundColor" :text-color="color.textColor" :active-text-color="color.activeColor"
+          unique-opened router :default-active="$route.path">
           <el-submenu :index="item.path +''" v-for="item in menuList" :key="item.id">
             <!-- 一级菜单模板区域 -->
             <template slot="title">
@@ -28,7 +29,9 @@
 
         </el-menu>
       </el-aside>
-      <el-main><router-view></router-view></el-main>
+      <el-main>
+        <router-view></router-view>
+      </el-main>
     </el-container>
   </el-container>
 
@@ -45,22 +48,25 @@
       return {
         //左侧菜单
         menuList: [],
-        color:{
-          activeColor:'#409bff',
-          textColor:'#fff',
-          backgroundColor:'#333744'
+        color: {
+          activeColor: '#409bff',
+          textColor: '#fff',
+          backgroundColor: '#333744'
         },
-        iconsObj:{
-          '125':'el-icon-user-solid',
-          '103':'el-icon-s-help',
-          '101':'el-icon-s-goods',
-          '102':'el-icon-s-order',
-          '145':'el-icon-s-marketing'
-        }
+        iconsObj: {
+          '125': 'el-icon-user-solid',
+          '103': 'el-icon-s-help',
+          '101': 'el-icon-s-goods',
+          '102': 'el-icon-s-order',
+          '145': 'el-icon-s-marketing'
+        },
+        //被激活的链接
+        // activePath:''
       }
     },
     created() {
       this.getMenuList()
+      // this.activePath = window.sessionStorage.getItem('activePath')
     },
     methods: {
       logout() {
@@ -75,7 +81,11 @@
         if (res.meta.status !== 200) return this.$message.error(res.meta.msg)
         this.menuList = res.data
         console.log(res);
-      }
+      },
+      //保存激活状态
+      // activeMenu(activePath){
+      //   window.sessionStorage.setItem('activePath',activePath)
+      // }
 
     }
   }
@@ -88,6 +98,7 @@
 
   .el-aside {
     background-color: #333744;
+
     .el-menu {
       border-right: none;
     }
